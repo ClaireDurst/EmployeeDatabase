77d9df0e395591557869d176beded8d091a119a4
ClaireDurst <clairedurst@gmail.com>
2017-08-08 12:06:19 -0400

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,18 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDRActcblTk603YwlL8SJIxxGtl8kL0NLk",
+    authDomain: "employeedatabase-6b05f.firebaseapp.com",
+    databaseURL: "https://employeedatabase-6b05f.firebaseio.com",
+    projectId: "employeedatabase-6b05f",
+    storageBucket: "",
+    messagingSenderId: "981650249963"
+  };
+  firebase.initializeApp(config);
+
+  var databaseref = firebase.database();
+
+  $("#submitbtn").on("click", function(){
+
+    
+
+  });
